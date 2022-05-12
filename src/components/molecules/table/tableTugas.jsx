d23ea9dc307f6a240161e3f0381ff5307926ef79
sarrahman <sarrahman.dev@gmail.com>
2022-05-12 12:39:15 +0800

    add hak akses page

--- src/components/molecules/table/tableTugas.jsx
+++ src/components/molecules/table/tableTugas.jsx
@@ -1,25 +1,87 @@
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material'
+import { Checkbox, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { useEffect, useState } from 'react'
 
-export default function TableTugas(){
-    return(
+export default function TableTugas() {
+    const [valueChecked, setValueChecked] = useState({
+        semua: false,
+        data: false,
+        karyawan: false,
+        pengaduan: false,
+        pengguna: false,
+    })
+
+    useEffect(() => {
+        console.log(valueChecked)
+    }, [valueChecked])
+
+    const handleChange = (event) => {
+        if (event.target.name === 'semua') {
+            setValueChecked({
+                semua: event.target.checked,
+                data: event.target.checked,
+                karyawan: event.target.checked,
+                pengaduan: event.target.checked,
+                pengguna: event.target.checked,
+            })
+        } else {
+            setValueChecked({
+                ...valueChecked,
+                [event.target.name]: event.target.checked,
+            })
+        }
+    }
+
+    return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }}>
-                <TableHead sx={{backgroundColor: 'background.main'}}>
+                <TableHead sx={{ backgroundColor: 'background.main' }}>
                     <TableRow>
-                        <TableCell rowSpan={1} colSpan={2}>Tugas</TableCell>
-                        <TableCell colSpan={4}>Deskripsi</TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell align="right">Master Data</TableCell>
-                        <TableCell align="right">Karyawan</TableCell>
-                        <TableCell align="right">Pengaduan</TableCell>
-                        <TableCell align="right">Pengguna</TableCell>
+                        <TableCell>Tugas</TableCell>
+                        <TableCell align='right'>Pilih Semua</TableCell>
+                        <TableCell align='right'>Data</TableCell>
+                        <TableCell align='right'>Karyawan</TableCell>
+                        <TableCell align='right'>Pengaduan</TableCell>
+                        <TableCell align='right'>Pengguna</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     <TableRow>
                         <TableCell>Tugas 1</TableCell>
-                        <TableCell>Deskripsi 1</TableCell>
+                        <TableCell align='right'>
+                            <Checkbox onChange={handleChange} name="semua" sx={{
+                                color: 'secondary.main', '&.Mui-checked': {
+                                    color: "primary.main",
+                                },
+                            }} />
+                        </TableCell>
+                        <TableCell align='right'>
+                            <Checkbox onChange={handleChange} checked={valueChecked.data} name="data" sx={{
+                                color: 'secondary.main', '&.Mui-checked': {
+                                    color: "primary.main",
+                                },
+                            }} />
+                        </TableCell>
+                        <TableCell align='right'>
+                            <Checkbox onChange={handleChange} checked={valueChecked.karyawan} name="karyawan" sx={{
+                                color: 'secondary.main', '&.Mui-checked': {
+                                    color: "primary.main",
+                                },
+                            }} />
+                        </TableCell>
+                        <TableCell align='right'>
+                            <Checkbox onChange={handleChange} checked={valueChecked.pengaduan} name="pengaduan" sx={{
+                                color: 'secondary.main', '&.Mui-checked': {
+                                    color: "primary.main",
+                                },
+                            }} />
+                        </TableCell>
+                        <TableCell align='right'>
+                            <Checkbox onChange={handleChange} checked={valueChecked.pengguna} name="pengguna" sx={{
+                                color: 'secondary.main', '&.Mui-checked': {
+                                    color: "primary.main",
+                                },
+                            }} />
+                        </TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
--- src/redux/action/auth.js
+++ src/redux/action/auth.js
@@ -1,31 +1,35 @@
 import api from "../../utils/api";
 
 export const loginApi = (username, password) => (dispatch) => {
-    dispatch({
-        type: 'LOADING',
-        value: true
-    });
-    return new Promise(async (resolve, reject) => {
-        const csrf = () => api.get('/csrf-cookie');
-        await csrf()
-        api.post('/login', {
-            username,
-            password
-        })
-            .then(res => {
-                dispatch({
-                    type: 'LOADING',
-                    value: false
-                });
-                resolve(res);
-            }).catch(err => {
-                dispatch({
-                    type: 'LOADING',
-                    value: false
-                });
-                reject(err);
-            })
-
-
-    })
-}
+  dispatch({
+    type: "LOADING",
+    value: true,
+  });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const csrf = () => api.get("/csrf-cookie");
+      await csrf();
+    } catch {
+      console.log("opps");
+    }
+    api
+      .post("/login", {
+        username,
+        password,
+      })
+      .then((res) => {
+        dispatch({
+          type: "LOADING",
+          value: false,
+        });
+        resolve(res);
+      })
+      .catch((err) => {
+        dispatch({
+          type: "LOADING",
+          value: false,
+        });
+        reject(err);
+      });
+  });
+};
